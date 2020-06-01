a1618b6424917f322ac5765f8be771dacbe924ad
Maciej Mandzak <maciekekseke@gmail.com>
2020-06-01 09:22:10 +0200

    Logic of Form Elements

--- todolistreact/src/formElements.js
+++ todolistreact/src/formElements.js
@@ -3,15 +3,19 @@ import React, {useState} from "react"
 export default function FormElements(props){
 
     const [taskValue, setTaskValue] = useState("");
+    const [taskPriority, setTaskPriority] = useState(1);
 
-    const handleChange = (event) => {
+    const handleChangeTask = (event) => {
         setTaskValue(event.target.value)
+    };
+    const handleChangePriority = (event) => {
+        setTaskPriority(event.target.value)
     };
         return (
             <form action="#">
-                <input onChange={(event) => handleChange(event)} className={"taskName"} type="text" placeholder={"Task Content"}/>
+                <input onChange={(event) => handleChangeTask(event)} className={"taskName"} type="text" placeholder={"Task Content"}/>
                 <span >Priority: </span>
-                <input className={"taskPriority"} type="number" min={1} max={10}/>
+                <input onChange={(event) => handleChangePriority(event)} className={"taskPriority"} type="number" min={1} max={10}/>
                 <button className={"addTaskBtn"}>Add Task To List</button>
             </form>
         )
